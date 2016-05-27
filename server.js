4a0439f13d17985003dbac8a236e98016ffd2299
William Hernandez Sierra <whs465@gmail.com>
2016-05-27 14:02:38 +0630

    routing out

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+  if (req.protocol === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
