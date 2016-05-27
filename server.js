f1f1f95801be892dac7d5926e6c3066db2284ba0
William Hernandez Sierra <whs465@gmail.com>
2016-05-27 13:25:12 +0630

    commenting app.get

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+// app.get('/', function (req, res) {   res.send('Hello World!'); });
+
 app.use(function (req, res, next) {
   if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
@@ -12,10 +14,6 @@ app.use(function (req, res, next) {
 });
 
 app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
 app.listen(PORT, function () {
   console.log('express server is up on port ' + PORT);
