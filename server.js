bd9b9012f60a111d27bb4317d538c0eade79b0a4
William Hernandez Sierra <whs465@gmail.com>
2016-05-27 13:59:38 +0630

    routing out

--- server.js
+++ server.js
@@ -3,19 +3,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
-});
-
 app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-  res.send(__dirname + '/public/index.html')
-});
 
 app.listen(PORT, function () {
   console.log('express server is up on port ' + PORT);
