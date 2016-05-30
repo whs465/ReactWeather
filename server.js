19e80b6cdcd80517c4cc41818f5b932c5f2dddba
William Hernandez Sierra <whs465@gmail.com>
2016-05-30 22:19:11 +0630

    changing to Public

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(function (req, res, next) {
   }
 });
 
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
 app.listen(PORT, function () {
   console.log('Express is up on port ' + PORT);
