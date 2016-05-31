ae1c3e13b4eac87ade3634f22a93b471df7de457
William Hernandez Sierra <whs465@gmail.com>
2016-05-31 11:42:40 +0630

    add grid system

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to examles page!</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few examples locations to try out</p>
+        <ol>
+          <li>
+            <Link to='/?location=Yangon'>Yangon, YG</Link>
+          </li>
+          <li>
+            <Link to='/?location=Bogota'>Bogota, BOG</Link>
+          </li>
+        </ol>
       </div>
     )
   };
